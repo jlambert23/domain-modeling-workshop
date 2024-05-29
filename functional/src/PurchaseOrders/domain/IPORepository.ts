3f93d88ece683f8c947c11129b926251fcb331b8
Justin Lambert <justin.lambert@synapsestudios.com>
2024-05-29 00:11:32 -0400

    convert PORepo constructor to function

--- functional/src/PurchaseOrders/domain/IPORepository.ts
+++ functional/src/PurchaseOrders/domain/IPORepository.ts
@@ -4,7 +4,7 @@ import { PONumber } from "./PONumber";
 import { PurchaseOrder } from "./PurchaseOrder";
 
 export interface IPORepository {
-  save: (po: PurchaseOrder) => ResultAsync<undefined, Error>;
+  save: (po: PurchaseOrder) => ResultAsync<null, Error>;
   fetch: (id: UUID) => ResultAsync<PurchaseOrder, Error>;
   fetchNextPONumber: (org: string) => ResultAsync<PONumber, never>;
 }
--- functional/src/PurchaseOrders/domain/PORepistory.ts
+++ functional/src/PurchaseOrders/domain/PORepistory.ts
@@ -4,35 +4,39 @@ import { IPORepository } from "./IPORepository";
 import { PONumber } from "./PONumber";
 import { PurchaseOrder } from "./PurchaseOrder";
 
-class PORepository implements IPORepository {
-  purchaseOrders: PurchaseOrder[] = [];
-  orgPONumberMap = new Map<string, number>();
-  private _validateParsedPONumber(parsed: {
+export const constructPORepository = (): IPORepository => {
+  const purchaseOrders: PurchaseOrder[] = [];
+  const orgPONumberMap = new Map<string, number>();
+
+  const validateParsedPONumber = (parsed: {
     prefix: string;
     num: number;
-  }): boolean {
-    const lastOrgPONumber = this.orgPONumberMap.get(parsed.prefix) ?? 0;
+  }): boolean => {
+    const lastOrgPONumber = orgPONumberMap.get(parsed.prefix) ?? 0;
     return parsed.num > lastOrgPONumber;
-  }
-  save(po: PurchaseOrder) {
-    if (!po.lineItems.length) return errAsync(new Error("Missing line items"));
-    const parsedPONumber = {
-      prefix: PONumber.prefix(po.poNumber),
-      num: PONumber.value(po.poNumber),
-    };
-    if (!this._validateParsedPONumber(parsedPONumber))
-      return errAsync(new Error("PO number is invalid"));
-    this.orgPONumberMap.set(parsedPONumber.prefix, parsedPONumber.num);
-    this.purchaseOrders.push(po);
-    return okAsync(undefined);
-  }
-  fetch(id: UUID) {
-    const po = this.purchaseOrders.find((p) => p.id === id);
-    return po ? okAsync(po) : errAsync(new Error("not found"));
-  }
-  fetchNextPONumber(org: string) {
-    const lastOrgPONumber = this.orgPONumberMap.get(org) ?? 0;
-    return okAsync(PONumber.create(org, lastOrgPONumber + 1));
-  }
-}
-export const constructPORepository = (): IPORepository => new PORepository();
+  };
+
+  return {
+    save(po: PurchaseOrder) {
+      if (!po.lineItems.length)
+        return errAsync(new Error("Missing line items"));
+      const parsedPONumber = {
+        prefix: PONumber.prefix(po.poNumber),
+        num: PONumber.value(po.poNumber),
+      };
+      if (!validateParsedPONumber(parsedPONumber))
+        return errAsync(new Error("PO number is invalid"));
+      orgPONumberMap.set(parsedPONumber.prefix, parsedPONumber.num);
+      purchaseOrders.push(po);
+      return okAsync(null);
+    },
+    fetch(id: UUID) {
+      const po = purchaseOrders.find((p) => p.id === id);
+      return po ? okAsync(po) : errAsync(new Error("not found"));
+    },
+    fetchNextPONumber(org: string) {
+      const lastOrgPONumber = orgPONumberMap.get(org) ?? 0;
+      return okAsync(PONumber.create(org, lastOrgPONumber + 1));
+    },
+  };
+};
